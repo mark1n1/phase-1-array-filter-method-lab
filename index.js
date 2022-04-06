e2dadb0fcde6eeeda174daa3b990f1029982fe71
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-04-05 18:23:23 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, matchExpression) {
+    return drivers.filter(function (driver) {
+        if(matchExpression === 'Sa'&& driver.includes(matchExpression)) {
+            return driver;
+        }
+        return null;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
